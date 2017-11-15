29e9c765939b57023a1651ba9906e41e79d38efc
Ken Li <iliawnek@gmail.com>
2017-11-15 21:02:17 +0000

    Pull out algorithm parameters

--- src/li/ken/Main.java
+++ src/li/ken/Main.java
@@ -24,18 +24,23 @@ public class Main {
             }
             reader.close();
 
-            // Remove noise.
+            // Algorithm parameters.
+            final int minClusterSize = 10;
+            final double minDiversity = 0.9;
+            final int[] windowSizes = {5, 30, 60};
+            final int threshold = 5;
+            final int filterSize = 20;
+            final int leniency = 3;
+            final int minBurstFactor = 2;
+
+            // Filter clusters.
             System.out.println("original: " + dataset.size());
-            dataset.filterClusterSize(10);
-            System.out.println("cluster size > 10: " + dataset.size());
-            dataset.filterClusterUserDiversity(0.90);
-            System.out.println("cluster user diversity > 0.9: " + dataset.size());
-            int threshold = 5;
-            int filterSize = 20;
-            int leniency = 3;
-            dataset.markBurstingClusters(5, threshold, filterSize, leniency, 2);
-            dataset.markBurstingClusters(30, threshold, filterSize, leniency, 2);
-            dataset.markBurstingClusters(60, threshold, filterSize, leniency, 2);
+            dataset.filterClusterSize(minClusterSize);
+            System.out.println("cluster size > " + minClusterSize + ": " + dataset.size());
+            dataset.filterClusterUserDiversity(minDiversity);
+            System.out.println("cluster user diversity > " + minDiversity + ": " + dataset.size());
+            for (int windowSize : windowSizes)
+                dataset.markBurstingClusters(windowSize, threshold, filterSize, leniency, minBurstFactor);
             dataset.filterNonBurstingClusters();
             System.out.println("non-bursting: " + dataset.size());
 
