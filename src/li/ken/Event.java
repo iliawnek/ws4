e47940d560ad22c06a45ccad8acb6efc78ebe22c
Ken Li <iliawnek@gmail.com>
2017-11-15 20:19:46 +0000

    Events only contain one cluster

--- src/li/ken/Event.java
+++ src/li/ken/Event.java
@@ -5,10 +5,9 @@ import java.util.ArrayList;
 // Represents a single event.
 // Contains a cluster of tweets related to the event.
 public class Event {
-    private ArrayList<Cluster> clusters;
+    private Cluster cluster;
 
     public Event(Cluster cluster) {
-        this.clusters = new ArrayList<>();
-        this.clusters.add(cluster);
+        this.cluster = cluster;
     }
 }
